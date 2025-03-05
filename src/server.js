9ad974771bad459cfd3cc3efd783b4cc0357be65
angel12213aa <clancy10112@gmail.com>
2025-03-05 10:10:46 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,27 +1,44 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connection from "./database.js";
+import routerUsuario from "./routers/usuario_router.js";
+import routerCliente from "./routers/cliente_router.js";
 
-import dotenv from "dotenv"
-import cors from "cors"
-import connection from "./database.js"
-
-import routerUsuario from "./routers/usuario_router.js"
-import routerCliente from "./routers/cliente_router.js"
-
-dotenv.config()
+dotenv.config();
 connection();
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 7001
-app.set("port", PORT)
-app.use(cors())
-app.use(express.json())
+const PORT = process.env.PORT || 7001;
+app.set("port", PORT);
 
+const allowedOrigins = [
+  "https://www.dominio1.com",
+  "https://www.dominio2.com",
+  "http://localhost:3000"
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Server On")
-})
+  res.send("Server On");
+});
 
-app.use("/api", routerUsuario)
-app.use("/api", routerCliente)
+app.use("/api", routerUsuario);
+app.use("/api", routerCliente);
 
 export default app;
